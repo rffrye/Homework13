94cf6f20c0e2844e8051177b75d4dc8782163243
RFFRYE <rffrye@users.noreply.github.com>
2020-12-04 22:47:40 -0500

    added models to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,29 @@
+// Import the ORM to create functions that will interact with the database.
 const orm = require("./config/orm");
 
-orm.selectAll("burgers");
+var burger = {
+	all: function (cb) {
+		orm.all('burgers', function (res) {
+			cb(res);
+		});
+	},
+	// The variables cols and vals are arrays.
+	create: function (cols, vals, cb) {
+		orm.create('burgers', cols, vals, function (res) {
+			cb(res);
+		});
+	},
+	update: function (objColVals, condition, cb) {
+		orm.update('burgers', objColVals, condition, function (res) {
+			cb(res);
+		});
+	},
+	delete: function (condition, cb) {
+		orm.delete('burgers', condition, function (res) {
+			cb(res);
+		});
+	}
+};
 
-orm.insertOne("burgers", "burger_name", "devoured",  );
-
-// orm.updateOne();
+// Export the database functions for the controller (catburgersController.js).
+module.exports = burger;
